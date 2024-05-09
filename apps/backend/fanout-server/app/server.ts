09a4da26169c7f1ed623ad3d3ee4563eef22f1a0
Koya Saka <k7oya14@gmail.com>
2024-05-09 21:30:46 +0900

    chore: Refactor server.ts to use Redis for user-list storage

--- apps/backend/fanout-server/app/server.ts
+++ apps/backend/fanout-server/app/server.ts
@@ -1,9 +1,8 @@
 import { prisma } from "./prisma"; // import your extended Prisma Client instance
 import { fetchUserInfo } from "./utils";
+import Redis from "ioredis";
 
-const Redis = require("ioredis");
-
-let redis = new Redis(process.env.REDIS_URL);
+const redis = new Redis(process.env.REDIS_URL!);
 
 async function main() {
   const subscription = await prisma.post.subscribe({
@@ -12,9 +11,9 @@ async function main() {
 
   for await (const event of subscription) {
     const userInfo = await fetchUserInfo(event.created.authorId);
-    const post = { ...event.created, author: userInfo };
-    console.log(post);
-    await redis.lpush("user-list", post);
+    const data = { ...event.created, author: userInfo };
+    console.log(data)
+    await redis.lpush("user-list", JSON.stringify(data));
   }
 }
 
