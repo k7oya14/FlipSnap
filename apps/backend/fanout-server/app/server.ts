3d43513bac47e4f00890de216ab14dba61705c80
Koya Saka <k7oya14@gmail.com>
2024-05-09 14:06:18 +0900

    set up redis

--- apps/backend/fanout-server/app/server.ts
+++ apps/backend/fanout-server/app/server.ts
@@ -1,6 +1,10 @@
 import { prisma } from "./prisma"; // import your extended Prisma Client instance
 import { fetchUserInfo } from "./utils";
 
+const Redis = require("ioredis");
+
+let redis = new Redis(process.env.REDIS_URL);
+
 async function main() {
   const subscription = await prisma.post.subscribe({
     create: {},
@@ -10,6 +14,7 @@ async function main() {
     const userInfo = await fetchUserInfo(event.created.authorId);
     const post = { ...event.created, author: userInfo };
     console.log(post);
+    await redis.lpush("user-list", post);
   }
 }
 
