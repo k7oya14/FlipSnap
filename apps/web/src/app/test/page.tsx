79d5c92ab8a837ad7bf652c6b13f813dde81dcba
Hikaru Ishiwata <hikaru160830@gmail.com>
2024-05-09 11:17:07 +0900

    test

---
+++ apps/web/src/app/test/page.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { isMobile } from "react-device-detect";
+
+const page = () => {
+  return (
+	<div>{isMobile?(<p>mobile</p>):(<p>notmobile</p>)}</div>
+  )
+}
+
+export default page
