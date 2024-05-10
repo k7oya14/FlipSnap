3327775105a08c64bd8fb7439ab99750ba4f365b
Koya Saka <k7oya14@gmail.com>
2024-05-10 11:50:39 +0900

    chore: Add wrangler.toml to .gitignore and update fetchLatestPosts handler

--- apps/backend/edge-api/src/lib/fetchLatestPosts.ts
+++ apps/backend/edge-api/src/lib/fetchLatestPosts.ts
@@ -3,14 +3,14 @@ import { Handler } from "hono";
 
 export const fetchLatestPosts: Handler = async (c) => {
   const take = Number(c.req.query("take"));
-  const cursorPostId = c.req.query("cursorPostId");
+  const skip = c.req.query("skip") ? Number(c.req.query("skip")) : 0;
 
   const redis = new Redis({
     url: c.env.UPSTASH_REDIS_REST_URL,
     token: c.env.UPSTASH_REDIS_REST_TOKEN,
   });
 
+  const posts = await redis.lrange("latest-posts", skip, skip + take - 1);
   
-
-
+  return c.json(posts);
 };
